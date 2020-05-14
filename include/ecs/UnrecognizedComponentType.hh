9816e53958f326cbfa71c0b74eb3d1f0ae0d5c81
Jacob Wirth <xthexder@users.noreply.github.com>
2020-05-13 23:04:14 -0700

    Fix returning pointer to stack address

--- include/ecs/UnrecognizedComponentType.hh
+++ include/ecs/UnrecognizedComponentType.hh
@@ -7,17 +7,21 @@
 class UnrecognizedComponentType : public std::invalid_argument
 {
 public:
-	UnrecognizedComponentType(std::type_index typeIndex)
-		: invalid_argument(""), typeIndex(typeIndex) {}
-	virtual const char *what() const throw()
+	UnrecognizedComponentType(std::type_index typeIndex) : invalid_argument(""), typeIndex(typeIndex)
 	{
 		std::stringstream ss;
 		ss << "component type " << string(typeIndex.name()) << " is not recognized. "
 		   << "Make sure you register it with EntityManager::RegisterComponentType.";
+		message = ss.str();
+	}
 
-		return ss.str().c_str();
+	virtual const char *what() const throw()
+	{
+
+		return message.c_str();
 	}
 
 private:
 	std::type_index typeIndex;
+	std::string message;
 };
